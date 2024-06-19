2f004d55566789480e6a503de678dc39d3ba7289
Harshit Bhardwaj <harshitbhardwaj97@gmail.com>
2024-06-19 17:47:41 +0530

    parseCSV() code added and test passed

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"encoding/csv"
+	"os"
+	"strconv"
+)
+
 type Grade string
 
 const (
@@ -21,7 +27,56 @@ type studentStat struct {
 }
 
 func parseCSV(filePath string) []student {
-	return nil
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	lineNumber := 0
+
+	var students []student
+
+	// Read and parse each line
+	for {
+
+		// Read each record from CSV
+		record, err := reader.Read()
+		
+		if err != nil {
+			break // Break if end of file
+		}
+
+		lineNumber++
+
+		if lineNumber == 1 {
+			continue // Skip the first line
+		}
+
+		currentStudent := student{}
+		currentStudent.firstName = record[0]
+		currentStudent.lastName = record[1]
+		currentStudent.university = record[2]
+
+		score1, _ := strconv.Atoi(record[3])
+		score2, _ := strconv.Atoi(record[4])
+		score3, _ := strconv.Atoi(record[5])
+		score4, _ := strconv.Atoi(record[6])
+
+		currentStudent.test1Score = score1
+		currentStudent.test2Score = score2
+		currentStudent.test3Score = score3
+		currentStudent.test4Score = score4
+
+		students = append(students, currentStudent)
+
+		// fmt.Println("Current student is", currentStudent)
+		// fmt.Println("-----------")
+	}
+	return students
 }
 
 func calculateGrade(students []student) []studentStat {
